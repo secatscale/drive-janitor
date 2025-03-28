9a2dfd7bf850b448a2ce3262e3e4e5e0d132dace
Devsstudent <devsstudent777@gmail.com>
2025-03-28 23:16:06 +0100

    make test cleaner

--- recursion_test.go
+++ recursion_test.go
@@ -44,21 +44,24 @@ func countSeparator(path string) int {
 	return len(count)
 }
 
-func getDepth(originalPath string, path string) int {
-	return countSeparator(path) - countSeparator(originalPath) + 1
+func getDepth(path string) int {
+	if (path == ".") {
+		return 0;
+	}
+	return countSeparator(path)
 }
 
-func isAboveMaxDepth(initialPath string, path string, maxDepth int) bool {
-	return getDepth(initialPath, path) > maxDepth
+func isAboveMaxDepth(path string, maxDepth int) bool {
+	return getDepth(path) > maxDepth
 }
 
 func (config *RecursionConfig) recurse(/* May take dectection and action struct*/) error {
 	initialPathFs := os.DirFS(config.InitialPath);
 	err := fs.WalkDir(initialPathFs, ".", func(path string, entry fs.DirEntry, err error) error {
-//		fmt.Println(getDepth(config.InitialPath, path), config.InitialPath, path, entry.Type().IsDir())
-		if (isAboveMaxDepth(config.InitialPath, path, config.MaxDepth)) {
+		if (isAboveMaxDepth(path, config.MaxDepth)) {
 			return fs.SkipDir;
 		}
+		fmt.Println(getDepth(path), config.InitialPath, path, entry.Type().IsDir())
 		if (!entry.Type().IsDir()) {
 			config.BrowseFiles += 1;
 		}
@@ -77,43 +80,27 @@ func (config *RecursionConfig) recurse(/* May take dectection and action struct*
 func TestRecursion(t *testing.T) {
 	dir := "test"
 	os.MkdirAll(dir, 0755)
-	os.MkdirAll("test/1", 0755)
+	dir_one := string(os.PathSeparator) + "1" + string(os.PathSeparator)
+	os.MkdirAll(dir + dir_one, 0755)
 	name := "file.txt"
-	path := "./"
-	_, err := os.Create(path + dir + "/" + name)
+	path, err :=  os.Getwd()
+	if (err != nil) {
+		t.Errorf("Error while getting the path: %v", err)
+	}
+	path = path + string(os.PathSeparator)
+	_, err = os.Create(path + dir + string(os.PathSeparator) + name)
 	if (err != nil) {
 		t.Errorf("Error while creating file: %v", err)
 	}
-	_, err = os.Create(path + dir + "/1/" + "file2.txt")
+	_, err = os.Create(path + dir + dir_one + "file2.txt")
 	if (err != nil) {
 		t.Errorf("Error while creating file: %v", err)
 	}
-	t.Run("Test max depth for Linux/Darwin", func(t *testing.T) {
-		testhelper.RunOSDependentTest(t, "Test max depth", func(t *testing.T) {
-			os.MkdirAll("test/1/2", 0755)
-			_, err = os.Create(path + dir + "/1/2/" + "file3.txt")
-			config := RecursionConfig{
-				InitialPath: "./test",
-				MaxDepth: 1,
-				SkipDirectories: []string{},
-				PriorityDirectories: []string{},
-				BrowseFiles: 0,
-			}
-			err = config.recurse()
-			if (err != nil) {
-				t.Errorf("Error while browsing files: %v", err)
-			}
-			if (config.BrowseFiles != 2) {
-				t.Errorf("Expected 2files, got %d", config.BrowseFiles)
-			}
-		}, map[string]bool{"linux": true, "darwin": true})
-	})
-
-	t.Run("Test Browsering for Linux/Darwin", func(t *testing.T) {
+	t.Run("Test Browsering", func(t *testing.T) {
 		testhelper.RunOSDependentTest(t, "Test Browsering", func(t *testing.T) {
 			config := RecursionConfig{
-				InitialPath: "./test",
-				MaxDepth: 1,
+				InitialPath: path + dir,
+				MaxDepth: 2,
 				SkipDirectories: []string{},
 				PriorityDirectories: []string{},
 				BrowseFiles: 0,
@@ -125,89 +112,33 @@ func TestRecursion(t *testing.T) {
 			if (config.BrowseFiles != 2) {
 				t.Errorf("Expected 2files, got %d", config.BrowseFiles)
 			}
-		}, map[string]bool{"linux": true, "darwin": true})
+		}, map[string]bool{"linux": true, "darwin": true, "windows": true})
 	});
 
-	t.Run("Test max depth Linux/Darwin", func(t *testing.T) {
-	testhelper.RunOSDependentTest(t, "Test max depth", func(t *testing.T) {
-		os.MkdirAll("test/1/2", 0755)
-		_, err = os.Create(path + dir + "/1/2/" + "file3.txt")
-		config := RecursionConfig{
-			InitialPath: "./test",
-			MaxDepth: 1,
-			SkipDirectories: []string{},
-			PriorityDirectories: []string{},
-			BrowseFiles: 0,
-		}
-		err = config.recurse()
-		if (err != nil) {
-			t.Errorf("Error while browsing files: %v", err)
-		}
-		if (config.BrowseFiles != 2) {
-			t.Errorf("Expected 2files, got %d", config.BrowseFiles)
-		}
-	}, map[string]bool{"linux": true, "darwin": true})});
+	t.Run("Test max depth", func(t *testing.T) {
+		testhelper.RunOSDependentTest(t, "Test max depth", func(t *testing.T) {
+			dir_two := "2" + string(os.PathSeparator)
+			os.MkdirAll(path + dir + dir_one + dir_two, 0755)
+			_, err = os.Create(path + dir + dir_one + dir_two + "file3.txt")
+			config := RecursionConfig{
+				InitialPath: path + dir,
+				MaxDepth: 2,
+				SkipDirectories: []string{},
+				PriorityDirectories: []string{},
+				BrowseFiles: 0,
+			}
+			err = config.recurse()
+			if (err != nil) {
+				t.Errorf("Error while browsing files: %v", err)
+			}
+			if (config.BrowseFiles != 2) {
+				t.Errorf("Expected 2files, got %d", config.BrowseFiles)
+			}
+		}, map[string]bool{"linux": true, "darwin": true, "windows": true})
+	})
 
-	t.Run("Test Browsering for Windows", func(t *testing.T) {
-	testhelper.RunOSDependentTest(t, "Test Browsering", func(t *testing.T) {
-		path, err := os.Getwd()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return
-	}
-		config := RecursionConfig{
-			InitialPath: path,
-			MaxDepth: 1,
-			SkipDirectories: []string{},
-			PriorityDirectories: []string{},
-			BrowseFiles: 0,
-		}
-		err = config.recurse()
-		if (err != nil) {
-			t.Errorf("Error while browsing files: %v", err)
-		}
-		if (config.BrowseFiles != 2) {
-			t.Errorf("Expected 2files, got %d", config.BrowseFiles)
-		}
-	}, map[string]bool{"windows": true})});
-
-
-	t.Run("Test max depth windows", func(t *testing.T) {
-	testhelper.RunOSDependentTest(t, "Test max depth", func(t *testing.T) {
-		path, err := os.Getwd()
-		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		os.MkdirAll(path + "\\1\\2", 0755)
-		_, err = os.Create(path + "\\1\\2\\" + "file3.txt")
-		config := RecursionConfig{
-			InitialPath: "path",
-			MaxDepth: 1,
-			SkipDirectories: []string{},
-			PriorityDirectories: []string{},
-			BrowseFiles: 0,
-		}
-		err = config.recurse()
-		if (err != nil) {
-			t.Errorf("Error while browsing files: %v", err)
-		}
-		if (config.BrowseFiles != 2) {
-			t.Errorf("Expected 2files, got %d", config.BrowseFiles)
-		}
-	}, map[string]bool{"windows": true})});
 
 	t.Cleanup(func() {
-		path, err := os.Getwd()
-		if (err != nil) {
-			fmt.Println("Error:", err)
-		}
-		if (WhichOs() == "windows") {
-			defer os.RemoveAll(path)
-
-		} else {
-			defer os.RemoveAll(path + dir + "/1/2")
-			defer os.RemoveAll(dir)
-		}
+		defer os.RemoveAll(path + dir)
 	})
 }
