8fdb4fbc6e036f14fde521c0c1e040bf8208a223
Juillard Thibault <thibault.juillard@ecole2600.com>
2025-04-10 19:52:42 +0200

    reorganise detection logic files

--- detection/checkage/checkage.go
+++ detection/checkage/checkage.go
@@ -1,4 +1,4 @@
-package main
+package checkage
 
 import (
 	"os"
--- detection/detection.go
+++ detection/detection.go
@@ -1,17 +1,13 @@
 package detection
 
 import (
-	"bufio"
-	"io"
-	"os"
-	"time"
-
-	"github.com/h2non/filetype"
+	"drive-janitor/detection/checkage"
+	"drive-janitor/detection/checktype"
 )
 
 func (detection DetectionConfig) FileTypeMatching(path string) (bool, error) {
 	// Call la function check type sur le path
-	fileType, err := CheckType(path)
+	fileType, err := checktype.CheckType(path)
 	if err != nil {
 		return false, err
 	}
@@ -29,7 +25,7 @@ func (detection DetectionConfig) FileAgeMatching(path string) (bool, error) {
 		return true, nil
 	}
 	// Call la function check age sur le path
-	age, err := CheckAge(path)
+	age, err := checkage.CheckAge(path)
 	if err != nil {
 		return false, err
 	}
@@ -39,72 +35,4 @@ func (detection DetectionConfig) FileAgeMatching(path string) (bool, error) {
 	}
 	// Call la function check age sur le path
 	return true, nil
-}
-
-func CheckType(filePath string) (fileType string, err error) {
-	//Stat rapide pour eviter les fichiers spéciaux
-	info, err := os.Lstat(filePath)
-	if err != nil {
-		return "", err
-	}
-	if !info.Mode().IsRegular() {
-		// Skip sockets, symlinks, devices, etc.
-		return "", nil
-	}
-
-	fd, err := os.Open(filePath)
-	// if permission denied, return empty string
-	if os.IsPermission(err) {
-		return "", nil
-	}
-	if err != nil {
-		return "", err
-	}
-	defer fd.Close()
-
-	reader := bufio.NewReader(fd)
-	buf := make([]byte, 24)
-	n, err := reader.Read(buf)
-	// if file is empty, return empty string
-	if err == io.EOF || n == 0 {
-		return "empty", nil
-	}
-	if err != nil {
-		return "", err
-	}
-	buf = buf[:n]
-	kind, err := filetype.Match(buf)
-	if err != nil {
-		return "", err
-	}
-	if kind == filetype.Unknown {
-		if isProbablyText(buf) {
-			return "text", nil
-		}
-		return "unknown", nil
-	}
-	return kind.MIME.Value, nil
-}
-
-func isProbablyText(buf []byte) bool {
-	for _, b := range buf {
-		if b == 0 {
-			return false
-		}
-		if (b < 32 && b != 9 && b != 10 && b != 13) || (b > 126 && b < 160) {
-			return false
-		}
-	}
-	return true
-}
-
-// CheckAge is a function that takes a file path and returns the age of the file in days.
-func CheckAge(filePath string) (int, error) {
-	info, err := os.Stat(filePath)
-	if err != nil {
-		return 0, err
-	}
-	age := time.Since(info.ModTime()).Hours() / 24
-	// fmt.Println(age)
-	return int(age), nil
 }
