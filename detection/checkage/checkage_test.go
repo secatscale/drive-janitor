cb05831d9792379fc6a8b56cf84c0dea9740bee3
Juillard Thibault <thibault.juillard@ecole2600.com>
2025-04-10 21:42:14 +0200

    fix broken test since reorganisation

--- detection/checkage/checkage_test.go
+++ detection/checkage/checkage_test.go
@@ -6,19 +6,20 @@ import (
 )
 
 func TestCheckAge(t *testing.T) {
+	age := 0
 	tests := []struct {
 		testname string
 		fileName string
 		age      int // expected age in days
 	}{
-		{"Test1", "sample.txt", 5},
-		{"Test2", "sample.zip", 5},
-		{"Test3", "sample.csv", 5},
-		{"Test4", "empty", 0},
+		{"Test1", "sample.txt", age},
+		{"Test2", "sample.zip", age},
+		{"Test3", "sample.csv", age},
+		{"Test4", "empty", age},
 	}
 	for _, tt := range tests {
 		t.Run(tt.testname, func(t *testing.T) {
-			filePath, err := filepath.Abs(filepath.Join("samples", tt.fileName))
+			filePath, err := filepath.Abs(filepath.Join("../../samples", tt.fileName))
 			got, err := CheckAge(filePath)
 			if err != nil {
 				t.Fatalf("Error: %v", err)
