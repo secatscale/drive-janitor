56331b3c51a2e5391c17f64acb9c35b8e7986690
Devsstudent <devsstudent777@gmail.com>
2025-04-24 13:20:06 +0200

    better code architecture

--- rules/rules.go
+++ rules/rules.go
@@ -1,26 +1,37 @@
 package rules
 
 import (
+	"drive-janitor/action"
 	"fmt"
 	"os"
 )
+
+func saveLog(action action.Action) {
+	err := action.GetLogFileName()
+	if err != nil {
+		fmt.Println("Error getting log file name:", err)
+		// Temporary exiting
+		os.Exit(1)
+	}
+	fmt.Printf("Log file: %s\n", action.LogConfig.LogRepository)
+
+	err = action.SaveToFile()
+	if err != nil {
+		fmt.Println("Error saving log file:", err)
+		// Temporary exiting
+		os.Exit(1)
+	}
+}
+
 func (r RulesArray) Loop() {
 	for _, rules := range r {
 		err := rules.Recursion.Recurse(rules.Detection, &rules.Action)
 		if err != nil {
 			panic(err)
 		}
-	err = rules.Action.GetLogFileName()
-	if err != nil {
-		fmt.Println("Error getting log file name:", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Log file: %s\n", rules.Action.LogConfig.LogRepository)
-
-	err = rules.Action.SaveToFile()
-	if err != nil {
-		fmt.Println("Error saving log file:", err)
-		os.Exit(1)
-	}
+		// Here also not mandatory
+		if (rules.Action.Log) {
+			saveLog(rules.Action)
+		}
 	}
 }
