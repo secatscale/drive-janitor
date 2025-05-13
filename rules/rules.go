a31a1c26be45ba7db25f36b42f3586bdbf3ee3ac
Devsstudent <devsstudent777@gmail.com>
2025-05-13 14:12:46 +0200

    added goroutine in rules loop

--- rules/rules.go
+++ rules/rules.go
@@ -3,6 +3,7 @@ package rules
 import (
 	"drive-janitor/action"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -25,13 +26,19 @@ func saveLog(action *action.Action) {
 
 func (r RulesArray) Loop() {
 	for _, rules := range r {
-		err := rules.Recursion.Recurse(rules.Detection, rules.Action)
-		if err != nil {
-			panic(err)
-		}
-		// Here also not mandatory
-		if rules.Action.Log {
-			saveLog(rules.Action)
-		}
+		go func(rules Rules) {
+			defer func() {
+				if r := recover(); r != nil {
+					log.Printf("Recovered panic in goroutine: %v", r)
+				}
+			}()
+			err := rules.Recursion.Recurse(rules.Detection, rules.Action)
+			if err != nil {
+				panic(err) // be careful using panic in goroutines
+			}
+			if rules.Action.Log {
+				saveLog(rules.Action)
+			}
+		}(rules)
 	}
 }
