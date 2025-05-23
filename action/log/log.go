6455400641ccebce141de38762fc0f64163ca560
Devsstudent <devsstudent777@gmail.com>
2025-05-23 14:43:21 +0200

    fix writing log same file within goroutine on windows

--- action/log/log.go
+++ action/log/log.go
@@ -1,65 +1,62 @@
 package log
 
 import (
+	"fmt"
 	"os"
 	"sync"
 
 	"github.com/gofrs/flock"
 )
 
-// Mutex global pour proteger l'acces concurentiel au fichier
+// Mutex global pour proteger l'acces concurrentiel au fichier
 var fileMutex sync.Mutex
 
 func SaveToFile(data string, logPath string) error {
-
-	// Verrou interne entre les goroutines
+	// Use internal mutex to serialize access within THIS process
 	fileMutex.Lock()
 	defer fileMutex.Unlock()
 
-	// Lock + ouverture du fichier
-	f, lock, err := loadFileWithlock(logPath)
-	if err != nil {
-		return err
-	}
-	defer closeFileAndUnlock(f, lock)
-
-	// Ecrire les donnees dans le fichier
-	_, err = f.WriteString(data)
-	if err != nil {
-		return err
-	}
-	return nil
+	// Now use flock for inter-process synchronization
+	return writeToFileWithLock(data, logPath)
 }
 
-func loadFileWithlock(logPath string) (*os.File, *flock.Flock, error) {
-	// Create a lock on the file using the new API
-	lock := flock.New(logPath)
+func writeToFileWithLock(data string, logPath string) error {
+	// Create lock for inter-process synchronization
+	lock := flock.New(logPath + ".lock") // Use separate lock file
 
-	// Try to lock the file
+	// Try to acquire the lock (this will wait if another process has it)
 	err := lock.Lock()
 	if err != nil {
-		return nil, nil, err
+		return fmt.Errorf("failed to acquire file lock: %w", err)
 	}
+	defer func() {
+		if unlockErr := lock.Unlock(); unlockErr != nil {
+			fmt.Printf("Warning: failed to unlock file: %v\n", unlockErr)
+		}
+	}()
 
-	// Open or create the file
+	// Now safely open and write to the actual file
 	f, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		lock.Unlock()
-		return nil, nil, err
+		return fmt.Errorf("failed to open file: %w", err)
+	}
+	defer func() {
+		if closeErr := f.Close(); closeErr != nil {
+			fmt.Printf("Warning: failed to close file: %v\n", closeErr)
+		}
+	}()
+
+	// Write data
+	_, err = f.WriteString(data)
+	if err != nil {
+		return fmt.Errorf("failed to write to file: %w", err)
 	}
 
-	return f, lock, nil
-}
+	// Sync to ensure data is written to disk
+	err = f.Sync()
+	if err != nil {
+		return fmt.Errorf("failed to sync file: %w", err)
+	}
 
-func closeFileAndUnlock(f *os.File, lock *flock.Flock) error {
-	err := f.Close()
-	if err != nil {
-		lock.Unlock()
-		return err
-	}
-	err = lock.Unlock()
-	if err != nil {
-		return err
-	}
 	return nil
 }
